52687643896f8e7baec455b730e3d9f39bcc55a3
Victor <viqtorkibz@gmail.com>
2024-01-16 13:07:57 +0300

    completed assynment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+
+  
